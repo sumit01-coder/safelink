0d51087fd1092528a4c7b5f00732229bbc2bc1e8
Sumit Patel <sumitpatel98894@gmail.com>
2026-08-30 22:16:34 +0530

    chore: bump version to v1.2.0 for OTA test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.safelink.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
