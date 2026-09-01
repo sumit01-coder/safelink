cb2d4fb97528833702699d3bb79fa8f4885095f5
Sumit Patel <sumitpatel98894@gmail.com>
2026-09-01 09:32:58 +0530

    fix: allow cleartext HTTP traffic in AndroidManifest to enable requests to http://192.168.4.1, bump to v1.4.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.safelink.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 11
-        versionName = "1.4.4"
+        versionCode = 12
+        versionName = "1.4.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
