5ebb3e61aae30d872e5ba96eacf53305082b6f4d
Sumit Patel <sumitpatel98894@gmail.com>
2026-09-02 09:13:09 +0530

    chore: bump version to 1.9.2 for relay rename bugfix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.safelink.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 35
-        versionName = "1.9.1"
+        versionCode = 36
+        versionName = "1.9.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
